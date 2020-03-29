dcc29465911cdb46b734eed65587b423793059e7
Edr2 <>
2020-03-29 09:40:49 +0300

    read/write from cluster

---
+++ main.py
@@ -0,0 +1,19 @@
+from cassandra.cluster import Cluster
+
+
+if __name__ == "__main__":
+    cluster = Cluster(['172.18.0.2'],port=9042)
+    session = cluster.connect()
+
+    session.execute("CREATE KEYSPACE IF NOT EXISTS test_keyspace WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 }")
+    session.execute('USE test_keyspace')
+
+    session.execute("CREATE TABLE IF NOT EXISTS users (username text,name text,age int,PRIMARY KEY(username));")
+    session.execute("INSERT INTO users(username,name,age) VALUES ('aali24','Ali Amin',34);")
+    session.execute("INSERT INTO users(username,name,age) VALUES ('jack01','Jack David',23);")
+    session.execute("INSERT INTO users(username,name,age) VALUES ('ninopk','Nina Rehman',34);")
+    
+
+    rows = session.execute('SELECT * FROM users')
+    for row in rows:
+        print(row.age,row.name,row.username)
